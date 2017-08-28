6262ae1e8679365de7aabf08818cf49c035479c2
Tim Siwula <tim.siwula@consensys.net>
2017-08-28 00:03:39 -0500

    added pegasys main command invocation

--- tools/cli/cli.go
+++ tools/cli/cli.go
@@ -1,22 +1,104 @@
-// make core cli
-// input --> docker
+/*
+		Date:					Sun Aug 27th
+		Author: 			Tim Siwula
+		File:					cli.go
+		Purpose:			A command line interperter tool.
+		Run:					go run cli.go run echo hello from from your cli.go
+		pegasys:			go run cli.go pegasys
+									go run cli.go pegasys docker
+
+
+*/
 
 package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"time"
 )
 
 func main() {
 
-	fmt.Println("Hey timbo")
-	day := time.Now().YearDay()
-	fmt.Println(day % 7)
+	//installPegasys()
+	dockerize()
+	// testing()
+
+}
+
+func installPegasys() {
+	// pwd := exec.Command("pwd")
+	// pwd.Run()
+	// fmt.Printf("pegasys pwd: %v\n", pwd)
+
+
+
+// to do: make sym link from:
+// /usr/local/bin/pegasys
+// to:
+// $GOPATH/cli.go
+
+
+
+
+// if on a mac:
+// copy file to: /usr/local/bin/cli.go
+// then get default shell:
+// then add alias: echo alias pegasys="go run cli.go pegasys" >> ~/.zsh
+// then update terminal: source ~/.zshrc
+// now run pegasys
+
+}
+
+func dockerize() {
+
+	switch os.Args[1] {
+	case "pegasys":
+			runDocker()
+	// defualt:
+	// 		panic("you did not enter the 'run' command. Currently that is all that is supported right now.")
+	}
+
+}
+
+func runDocker() {
+	fmt.Printf("pegasys will now run: %v\n", os.Args[2:])
+	command := exec.Command(os.Args[2], os.Args[3:]...)
+	// command := exec.Command("docker login")
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	//command.SysProcAttr = &syscall.SysProcAttr{
+	// 	Cloneflags: syscall.CLONE_NEWUTS,
+	// }
+	command.Run()
+	//assert(command.Run())
+}
+
+func assert(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func testing() {
+	fmt.Println("testing function")
+	day := time.Now()
+	fmt.Println(day)
 }
 
 /*
 	cli features --
+
+	pegasys.cli() arguments/flags:
+			os,
+			db,
+			privacy,
+			network,
+			client-light?,
+			vm,
+			key management,
 
   a general purpose command line interface that will let you spawn a custom
 	component architecture.
@@ -26,9 +108,6 @@ func main() {
 	step2 expose 		-- interface to the evm with this program a la desemmbler.
 	step3 spawn 		-- allow this program/tool to communicate with itself on other comps
 	step4 repead
-
-  functions: cli() -
-			flags:	os, db, privacy, network
 
 */
 
